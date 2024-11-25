60a661e41070a1fd6aadb461f147740995b4c4cb
Alan Teesdale <78932086+TEESDAAL@users.noreply.github.com>
2024-11-25 13:43:42 +1300

    added more complex model

---
+++ first_attempt/simple_pred/MLGP_main_threading.py
@@ -0,0 +1,52 @@
+#python packages
+import random
+import time
+import numpy as np
+import eval_gp
+# deap package
+from deap import tools, algorithms
+from parameters import  population, cxProb, mutProb, elitismProb, generations
+from toolbox import toolbox
+import multiprocessing
+from scoop import futures
+randomSeeds = 12
+
+
+def main(randomSeeds):
+    random.seed(randomSeeds)
+    pop = toolbox.population(population)
+    hof = tools.HallOfFame(1)
+    log = tools.Logbook()
+    stats_fit = tools.Statistics(key=lambda ind: ind.fitness.values)
+    stats_size = tools.Statistics(key=len)
+    mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+    mstats.register("avg", np.mean)
+    mstats.register("std", np.std)
+    mstats.register("min", np.min)
+    mstats.register("max", np.max)
+    log.header = ["gen", "evals"] + mstats.fields
+
+    pop, log, hof2 =  eval_gp.eaSimple(pop, toolbox, cxProb, mutProb, elitismProb, generations, stats=mstats, halloffame=hof, verbose=True)
+    #pop, log = algorithms.eaSimple(pop, toolbox, cxProb, mutProb, generations, stats=mstats, halloffame=hof, verbose=True)
+    return pop, log, hof, hof2
+
+
+
+
+if __name__ == "__main__":
+    pool = multiprocessing.Pool()
+    toolbox.register("map", pool.map)
+    # toolbox.register("map", futures.map)
+    beginTime = time.process_time()
+    pop, log, hof, hof2 = main(randomSeeds)
+    endTime = time.process_time()
+    trainTime = endTime - beginTime
+
+    testResults = toolbox.test(hof2[0])
+    testTime = time.process_time() - endTime
+
+    print('Best individual ', hof[0])
+    print('Test results  ', testResults)
+    print('Train time  ', trainTime)
+    print('Test time  ', testTime)
+    print('End')
---
+++ first_attempt/simple_pred/toolbox.py
@@ -0,0 +1,70 @@
+
+from collections.abc import Callable
+from deap import base, creator, gp
+from deap import tools
+import gp_restrict
+from function_set import pset
+from parameters import initialMaxDepth, initialMinDepth, maxDepth
+import numpy as np
+import math
+from make_datasets import x_train, y_train, x_validation, y_validation, x_test, y_test
+import operator
+from scoop import futures
+import multiprocessing
+
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+
+toolbox = base.Toolbox()
+
+
+
+toolbox.register("expr", gp_restrict.genHalfAndHalfMD, pset=pset, min_=initialMinDepth, max_=initialMaxDepth)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("compile", gp.compile, pset=pset)
+
+def squared_distance(t1: tuple[float, float], t2: tuple[float, float]) -> float:
+    return (float(t1[0]) - float(t2[0]))**2 + (float(t1[1]) - float(t2[1]))**2
+
+def evaluate(individual: gp.PrimitiveTree, compiler: Callable[[gp.PrimitiveTree], Callable], x_train: np.ndarray, y_train: np.ndarray) -> tuple[float]:
+    """Compute the MSE of the distances between the models answer and the true (val, aro) pair avoids computing the sqrt"""
+    model = compiler(individual)
+    # calculate errors by MSE, error of each model given by geometric distance between values (pythag)
+    square_errors = [
+        squared_distance(model(x), y) for x, y in zip(x_train, y_train)
+    ]
+
+    return sum(square_errors) / len(square_errors),
+
+
+toolbox.register("evaluate", evaluate, compiler=toolbox.compile, x_train=x_train, y_train=y_train)
+toolbox.register("validation", evaluate, compiler=toolbox.compile, x_train=x_validation, y_train=y_validation)
+toolbox.register("test", evaluate, compiler=toolbox.compile, x_train=x_test, y_train=y_test)
+
+# def evaluate(individual: gp.PrimitiveTree, x_train: np.ndarray, y_train: np.ndarray) -> tuple[float]:
+#     model = toolbox.compile(individual)
+#
+#     square_errors = [(model(img) - float(val))**2 for img, (val, arousal) in zip(x_train, y_train)]
+#     return math.sqrt(sum(square_errors) / len(square_errors)),
+#
+#
+# toolbox.register("evaluate", evaluate, x_train=x_train, y_train=y_train)
+# toolbox.register("validation", evaluate, x_train=x_validation, y_train=y_validation)
+# toolbox.register("test", evaluate, x_train=x_test, y_train=y_test)
+
+
+# toolbox.register("select", tools.selTournament, tournsize=7)
+toolbox.register("select", tools.selDoubleTournament,
+                           fitness_size=7,
+                           parsimony_size=1.4,
+                           fitness_first=False)
+toolbox.register("selectElitism", tools.selBest)
+toolbox.register("mate", gp.cxOnePoint)
+toolbox.register("expr_mut", gp_restrict.genFull, min_=0, max_=2)
+toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=maxDepth))
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=maxDepth))
+
+
