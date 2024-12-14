c73a2ec8ab43f8f3124206211e1f22f82f90f5f7
teesdaalan@myvuw.ac.nz <teesdaalan@greta-pt.ecs.vuw.ac.nz>
2024-12-14 20:00:21 +1300

    results from no LBP, and updated gitignore

---
+++ second_attempt/shared_tools/parameters.py
@@ -0,0 +1,15 @@
+#from make_datasets import x_train
+
+population = 50
+generations = 5
+cxProb = 0.8
+mutProb = 0.19
+elitismProb = 0.01
+totalRuns = 1
+initialMinDepth = 3
+initialMaxDepth = 10
+maxDepth = 20
+
+tournament_size = 7
+parsimony_size = 1.5
+fitness_first = False
