3f31333ad007886dcfbc2fc7b6aa545241e6b04a
teesdaalan@myvuw.ac.nz <teesdaalan@greta-pt.ecs.vuw.ac.nz>
2024-12-15 15:25:42 +1300

    new results

--- changing_fitness/Job.sh
+++ changing_fitness/Job.sh
@@ -31,8 +31,8 @@ else
 fi
 
 model=$1
-homeDir='/vol/grid-solar/sgeusers/teesdaalan/SummerResearch/performant_refactor'
+homeDir='/vol/grid-solar/sgeusers/teesdaalan/SummerResearch/changing_fitness'
 
 cd $homeDir
 
-python -m scoop model_selector.py $model -s $SGE_TASK_ID -p 100 -g 75
+python -m scoop model_selector.py $model -s $SGE_TASK_ID -p 100 -g 100
