25b4c0e94b6a969ead0bab27d3a6cef90658431b
Alan Teesdale <78932086+TEESDAAL@users.noreply.github.com>
2025-01-13 13:52:59 +1300

    added classification attempt

---
+++ idgp_classification/shared_tools/make_datasets.py
@@ -0,0 +1,15 @@
+import numpy as np
+BASE_PATH = "../datasets"
+# READ_PATH = f"{BASE_PATH}/catagory_balanced_datasets"
+#READ_PATH = f"{BASE_PATH}/even_datasets"
+#READ_PATH = f"{BASE_PATH}/JAFFE"
+#READ_PATH = f"{BASE_PATH}/vallessJAFFE"
+READ_PATH = f"{BASE_PATH}/happy_sad_dataset"
+
+x_train = np.load(f"{READ_PATH}/x_train.npy") / 255
+y_train = np.load(f"{READ_PATH}/y_train.npy")
+x_validation = np.load(f"{READ_PATH}/x_val.npy") / 255
+y_validation = np.load(f"{READ_PATH}/y_val.npy")
+x_test = np.load(f"{READ_PATH}/x_test.npy") / 255
+y_test = np.load(f"{READ_PATH}/y_test.npy")
+
